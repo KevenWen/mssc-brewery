a5030932f95d8a69b8aa425c846e5bb42567c475
KevenWen <10082511@qq.com>
2021-01-03 18:50:51 +0800

    adding post example

--- src/main/java/keven/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/keven/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package keven.springframework.msscbrewery.web.controller;
 
 import keven.springframework.msscbrewery.services.BeerService;
 import keven.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -25,7 +23,16 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerID) {
 
         return new ResponseEntity<>(beerService.getBeerById(beerID), HttpStatus.OK);
-
     }
 
+    @PostMapping // POST - create new beer
+    public ResponseEntity handlePost(BeerDto beerDto){
+
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //todo  add hostname to url: http://localhost:8080
+        headers.add("Location","/api/v1/beer/" + savedDto.getId().toString());
+        return new ResponseEntity(headers,HttpStatus.CREATED);
+    }
 }
