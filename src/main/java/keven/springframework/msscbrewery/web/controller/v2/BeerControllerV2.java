138a7d6e21de6a04f9373f94afacb335fa02e8d4
KevenWen <10082511@qq.com>
2021-02-14 20:31:49 +0800

    Lombok values.

--- src/main/java/keven/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/keven/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -2,29 +2,29 @@ package keven.springframework.msscbrewery.web.controller.v2;
 
 import keven.springframework.msscbrewery.services.V2.BeerService2;
 import keven.springframework.msscbrewery.web.model.V2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-@Validated
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerV2 {
 
     private final BeerService2 beerServicev2;
-
-    public BeerControllerV2(BeerService2 beerServicev2) {
-        this.beerServicev2 = beerServicev2;
-    }
+//
+//    public BeerControllerV2(BeerService2 beerServicev2) {
+//        this.beerServicev2 = beerServicev2;
+//    }
 
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDtoV2> getBeer(@NotNull @PathVariable("beerId") UUID beerID) {
@@ -35,9 +35,9 @@ public class BeerControllerV2 {
     @PostMapping // POST - create new beer
     public ResponseEntity handlePost(@Valid @NotNull @RequestBody BeerDtoV2 beerDtov2){
 
-        BeerDtoV2 savedDto = beerServicev2.saveNewBeer(beerDtov2);
-
-        HttpHeaders headers = new HttpHeaders();
+        log.debug("in handle post......");
+        val savedDto = beerServicev2.saveNewBeer(beerDtov2);
+        var headers = new HttpHeaders();
         //todo  add hostname to url: http://localhost:8080
         headers.add("Location","/api/v1/beer/" + savedDto.getId().toString());
         return new ResponseEntity(headers,HttpStatus.CREATED);
